0d7cb7e206cd30eeae82004170831b38ad076604
daheeKang25 <2501857@student.hywoman.ac.kr>
2026-05-07 19:11:21 +0900

    Update app.py

--- app.py
+++ app.py
@@ -946,9 +946,11 @@ def internal_error(error):
 # =========================================================
 
 if __name__ == "__main__":
+    port = int(os.getenv("PORT", 5000))
+
     app.run(
-        host="127.0.0.1",
-        port=5000,
-        debug=True,
+        host="0.0.0.0",
+        port=port,
+        debug=False,
         use_reloader=False
     )
